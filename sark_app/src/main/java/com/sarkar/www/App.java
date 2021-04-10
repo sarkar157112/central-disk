28b4cddad4fee866c435ca56e63812803438d5fa
sark <sark@gmail.com>
2021-04-10 06:01:02 +0000

    new code

---
+++ sark_app/src/main/java/com/sarkar/www/App.java
@@ -0,0 +1,13 @@
+package com.sarkar.www;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Sarkar!" );
+    }
+}
